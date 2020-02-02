57981e1d21681fad1e5f8da2d4cf1625fdf1938c
Nutthapat Piasangka <n.devs260340@gmail.com>
2020-02-03 01:06:15 +0700

    update

--- common/request.js
+++ common/request.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-export async function POST(url = '', data = {}) {
+export async function POST(url = '',mode = '', data = {}) {
     // Default options are marked with *
     const response = await fetch(url, {
         method: 'POST', // *GET, POST, PUT, DELETE, etc.
-        mode: 'cors', // no-cors, *cors, same-origin
+        mode: `${mode !== '' ? mode : "cors"}`, // no-cors, *cors, same-origin
         cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
         credentials: 'same-origin', // include, *same-origin, omit
         headers: {
@@ -24,11 +24,11 @@ export async function GET(url = '') {
     return await response.json(); // parses JSON response into native JavaScript objects
 }
 
-export async function PUT(url = '', data = {}) {
+export async function PUT(url = '',mode = '', data = {}) {
     // Default options are marked with *
     const response = await fetch(url, {
         method: 'PUT', // *GET, POST, PUT, DELETE, etc.
-        mode: 'cors', // no-cors, *cors, same-origin
+        mode: `${mode !== '' ? mode : "cors"}`, // no-cors, *cors, same-origin
         cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
         credentials: 'same-origin', // include, *same-origin, omit
         headers: {
